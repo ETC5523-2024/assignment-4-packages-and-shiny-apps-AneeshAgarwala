090f0022cccdc71cb4b64e47fff3828024f3d780
AneeshAgarwala <aaga0022@student.monash.edu>
2024-10-23 23:03:54 +1100

    Package Template

---
+++ data-raw/asx_list.R
@@ -0,0 +1,4 @@
+## code to prepare `asx_list` dataset goes here
+
+usethis::use_data(asx_list, overwrite = TRUE)
+
---
+++ data/asx_list.R
@@ -0,0 +1,15 @@
+#' ASX Listed Companies Directory Data
+#'
+#' A complete list of companies listed on the Australian Stock Exchange as in October 2024.
+#'
+#' @format ## 'asx'
+#' A data frame with 1943 rows and 5 columns:
+#' \describe{
+#'    \item{ASX code}{3 digit code for the listed company}
+#'    \item{Company name}{Full Name of the listed company}
+#'    \item{GICs industry group}{Industry Classification for the listed company}
+#'    \item{Listing data}{Date the company got listed on exchange}
+#'    \item{Market Cap}{Market Capitalisation of the listed company as on 20th October 2024}
+#' }
+#' @source <https://www.asx.com.au/markets/trade-our-cash-market/directory>
+
